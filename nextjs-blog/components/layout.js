9ebd3ae765e686f1b8231fa8fcecf008d331342e
Kristofer Krause <kris.krause@gmail.com>
2021-10-02 17:47:04 -0400

    files

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -13,8 +13,13 @@ export default function Layout({ children, home }) {
                 <link rel="icon" href="/favicon.ico" />
                 <meta
                     name="description"
-                    content="Learn how to build a personal website using Next.js"
+                    content="Software development blog by Kris Krause"
                 />
+
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
 
                 <meta name="og:title" content={siteTitle} />
                 <meta name="twitter:card" content="summary_large_image" />
